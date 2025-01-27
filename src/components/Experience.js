ab134a11aeb9ece3e5c6694e28e9af5cfb04d1a3
Pikalot <pikalot@hotmail.com>
2025-01-26 20:36:05 -0800

    Fixed phone screen output with picture

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,8 +9,11 @@ export default function Experience({exp, education}) {
                     <li key={index}>
                         <h2>{job.title}</h2>
                         <h3>{job.name}</h3>
-                        <p>{job.from_date} {job.to_date}</p>
-                        <p>{job?.city + ', '} {job?.state + ', '} {job.country}</p>
+                        <p>{job?.from_date ? job.from_date + " - " : ""}
+                            {job?.to_date ? job?.to_date : ""}</p>
+                        <p>{job?.city ? job?.city + ', ' : ""}
+                            {job?.state ? job?.state + ', ' : ""}
+                            {job?.country ? job?.country : ""}</p>
                     </li>
                 )) :
                 "N/A"}
@@ -24,7 +27,9 @@ export default function Experience({exp, education}) {
                     <h3>{school?.degree ? school?.degree + ", " : ""}{school?.major ? school?.major : ""}</h3>
                     <p>Graduation Year (Completed/Expected): {school?.grad_year}</p>
                     <p>Grades: {school?.gpa ? school?.gpa : "N/A"}</p>
-                    <p>{school?.city + ', '}{school?.state + ', '}{school?.country}</p>
+                    <p>{school?.city ? school?.city + ', ' : ""}
+                        {school?.state ? school?.state + ', ': ""}
+                        {school?.country ? school?.country : ""}</p>
                     </li>
                 )) :
                 "N/A"}
