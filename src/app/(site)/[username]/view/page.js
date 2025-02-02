e7d2119c057a713ef754dca41b6ff23607a0307a
Pikalot <pikalot@hotmail.com>
2025-02-02 05:05:41 -0800

    add back findAllMembers

--- src/app/(site)/[username]/view/page.js
+++ src/app/(site)/[username]/view/page.js
@@ -14,11 +14,11 @@ export default async function ViewPage({params}) {
     const facebook = await getSocialMedia(userId, 'Facebook');
     const twitter = await getSocialMedia(userId, 'Twitter');
     const linkedin = await getSocialMedia(userId, 'LinkedIn');
-    // const members = await findAllMembers();
+    const members = await findAllMembers();
 
     return (
         <div>
-            {/* <MemberPage members={members} /> */}
+            <MemberPage members={members} />
             <Footer 
                       member={member[0]} 
                       github={github? github[0] : ''} 
