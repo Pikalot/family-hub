f6e4902b2f5abb486d1d98df92e8aa8bd40a8b95
Pikalot <pikalot@hotmail.com>
2025-02-24 19:13:55 -0800

    Swapped email and phone fields

--- src/app/(site)/account-settings/ClientAccountSettings.js
+++ src/app/(site)/account-settings/ClientAccountSettings.js
@@ -220,33 +220,6 @@ export default function ClientAccountSettings() {
                             </div>
                         </div>
 
-                        {/* Email Input */}
-                        <div className={styles["editor-block"]}>
-                            <label>Email</label>
-                            <div className={styles["block-container"]}>
-                                <div className={styles["field-input"]}>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="h-4 w-4 opacity-70 fill-black dark:fill-current"
-                                        viewBox="0 0 16 16"
-                                    >
-                                        <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                                        <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                                    </svg>
-                                    <input
-                                        type="text"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        // disabled={!isEditing}
-                                        placeholder={session?.user.email}
-                                        className={`${styles["field"]} 
-                                            ${isEditing ? "text-white" : "text-black"}`}
-                                        autoComplete="off"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-
                         {/* Phone Input */}
                         <div className={styles["editor-block"]}>
                             <label>Phone</label>
@@ -267,6 +240,33 @@ export default function ClientAccountSettings() {
                                         onChange={(e) => setPhone(e.target.value)}
                                         // disabled={!isEditing}
                                         placeholder="(000) 000-0000"
+                                        className={`${styles["field"]} 
+                                            ${isEditing ? "text-white" : "text-black"}`}
+                                        autoComplete="off"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Email Input */}
+                        <div className={styles["editor-block"]}>
+                            <label>Email</label>
+                            <div className={styles["block-container"]}>
+                                <div className={styles["field-input"]}>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-4 w-4 opacity-70 fill-black dark:fill-current"
+                                        viewBox="0 0 16 16"
+                                    >
+                                        <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                                        <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                                    </svg>
+                                    <input
+                                        type="text"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        // disabled={!isEditing}
+                                        placeholder={session?.user.email}
                                         className={`${styles["field"]} 
                                             ${isEditing ? "text-white" : "text-black"}`}
                                         autoComplete="off"
